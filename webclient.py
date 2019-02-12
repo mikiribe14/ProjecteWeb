c08cc25ce21f563093737efebacb20990377e986
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-02-12 12:56:41 +0100

    Prova web-scrapping

---
+++ webclient.py
@@ -0,0 +1,35 @@
+#download a web page
+#searche activities in the web page
+#print the activities
+
+from urllib.request import urlopen
+
+class WebClient(object):
+    """WebVlient class"""
+    def __init__(self):
+        super(WebClient, self).__init__()
+
+
+
+    def download_page(arg):
+        #connect to the web site
+        file=urlopen("http://www.eps.udl.cat/ca/")
+        #get the download download_page
+        page=file.read()
+        #close the connection
+        file.close()
+        return page
+
+
+    def run(self):
+        #download a web page
+        page=self.download_page()
+
+        #print it
+        print(page)
+
+
+
+if __name__=="__main__":
+    c = WebClient()
+    c.run()
