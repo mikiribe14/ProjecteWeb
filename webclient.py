55a5b21a54042f7ed9909f7b476e1f5b37c8eabf
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-02-19 12:13:09 +0100

    Print activities not successfull

--- webclient.py
+++ webclient.py
@@ -3,12 +3,13 @@
 #print the activities
 
 from urllib.request import urlopen
+import bs4 #li dono un fitxer xml i em construeix un arbre
+    #HTML es un exemple concret de xml, si està ben fet
 
 class WebClient(object):
-    """WebVlient class"""
+    """WebClient class"""
     def __init__(self):
         super(WebClient, self).__init__()
-
 
     def download_page(arg):
         #connect to the web site
@@ -19,18 +20,27 @@ class WebClient(object):
         file.close()
         return page
 
+    def search_activities(self,page):
+        tree = bs4.BeautifulSoup(page,"lxml")
+        activities = tree.find_all("div","features-links-item")
+        act_list=[]
+        for activity in activities:
+            title=activity.find("span","flink-title")
+            link=activity.find("a")
+            act_list.append(title.text,link["href"])
+        return act_list
 
     def run(self):
         #download a web page
         page=self.download_page()
-
         #print it
-        print(page)
-
-
+        #print(page)
+        #search activities in the page
+        data=self.search_activities(page)
+        #print the activities
+        print(data)
 
 if __name__=="__main__":
     c = WebClient()
     c.run()
-    print "prova de github desde portatil"
-    print "prova 2 desde portatil"
+
